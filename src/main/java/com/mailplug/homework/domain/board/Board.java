b186f8808d117406527f34d4ac63f431ecc88f87
alexgim <alexgim961101@gmail.com>
2023-08-13 00:38:20 +0900

    [01] Entity 기본 설계

--- src/main/java/com/mailplug/homework/domain/board/Board.java
+++ src/main/java/com/mailplug/homework/domain/board/Board.java
@@ -1,13 +1,19 @@
 package com.mailplug.homework.domain.board;
 
 import com.mailplug.homework.domain.BaseEntity;
+import com.mailplug.homework.domain.boardDetail.BoardDetail;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "board")
 public class Board extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(nullable = false)
     private String title;
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+    private List<BoardDetail> boardDetails = new ArrayList<>();
 }
--- src/main/java/com/mailplug/homework/domain/boardDetail/BoardDetail.java
+++ src/main/java/com/mailplug/homework/domain/boardDetail/BoardDetail.java
@@ -1,6 +1,7 @@
 package com.mailplug.homework.domain.boardDetail;
 
 import com.mailplug.homework.domain.BaseEntity;
+import com.mailplug.homework.domain.board.Board;
 import jakarta.persistence.*;
 
 @Entity
@@ -8,7 +9,13 @@ import jakarta.persistence.*;
 public class BoardDetail extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String username;
+    @Column(nullable = false)
     private String content;
+    private Long count;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Board board;
 }
